3a0a4402848122b7d4b7a4572aa5697f57c3aa7e
Claudia Falk <claudiafalk0@gmail.com>
2019-11-06 14:38:24 -0700

    added styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
 import * as serviceWorker from './serviceWorker';
-import css from "./styles/css/index.css";
+import "./styles/css/index.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
